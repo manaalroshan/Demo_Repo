105be27b8aa9167ab1544dc5fa42f5688edb8c89
Manaal Roshan <manaalroshan@gmail.com>
2026-05-08 20:46:55 +0530

    Added multiply function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import add, subtract
+from utils import add, subtract, multiply
 from logger import log_operation
 
 def run():
@@ -9,6 +9,9 @@ def run():
 
     result2 = subtract(a, b)
     log_operation("subtract", a, b, result2)
+    
+    result3 = multiply(a, b)
+    log_operation("multiply", a, b, result3)
 
 if __name__ == "__main__":
     run()
